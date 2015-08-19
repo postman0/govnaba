4556ec78744fa77e3feb1e15f21026f1aa8847eb
postman0 <postman0@users.noreply.github.com>
2015-08-19 20:19:19 +0300

    fix for the previous fix

--- server/src/govnaba/post_processors.go
+++ server/src/govnaba/post_processors.go
@@ -119,9 +119,12 @@ func SageProcessorBefore(cl *Client, p *Post) error {
 func SageProcessorAfter(cl *Client, p *Post) error {
 	_, saged := p.Attrs.Get("sage")
 	if !saged {
-		cl.db.Exec(`UPDATE threads SET last_bump_date = DEFAULT 
-			WHERE id = (SELECT thread_id FROM posts WHERE board_local_id = $1)
+		_, err := cl.db.Exec(`UPDATE threads SET last_bump_date = DEFAULT 
+			WHERE id IN (SELECT thread_id FROM posts WHERE board_local_id = $1)
 			AND board_id = (SELECT id FROM boards WHERE name = $2);`, p.ThreadId, p.Board)
+		if err != nil {
+			log.Printf("SageProcessorAfter: %s", err)
+		}
 	}
 	return nil
 }
